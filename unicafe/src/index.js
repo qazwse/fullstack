7d2c711e942770b28b42b541d39bd9d002c0574c
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-05-19 16:17:55 -0400

    1.6 complete + learning state added whoops

---
+++ unicafe/src/index.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+const Section = ({title}) => (
+  <h1>
+    {title}
+  </h1>
+)
+
+const Button = ({onclick, text}) => (
+  <button onClick={onclick}>{text}</button>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good+1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral+1)
+  }
+  const handleBadClick = () => {
+    setBad(bad+1)
+  }
+
+  return (
+    <div>
+      <Section title='give feedback' />
+      <Button onclick={handleGoodClick} text='good' />
+      <Button onclick={handleNeutralClick} text='neutral' />
+      <Button onclick={handleBadClick} text='bad' />
+      <Section title='stats' />
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>good {bad}</p>
+      
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
