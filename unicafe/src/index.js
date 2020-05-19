e843ba36afea60f46e9c0fc5b4c93e7a49d51d8e
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-05-19 16:22:58 -0400

    1.7 finished

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -11,6 +11,8 @@ const Button = ({onclick, text}) => (
   <button onClick={onclick}>{text}</button>
 )
 
+
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -35,7 +37,11 @@ const App = () => {
       <Section title='stats' />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
-      <p>good {bad}</p>
+      <p>bad {bad}</p>
+      <p>all {good+neutral+bad}</p>
+      <p>average {(good-bad)/(good+neutral+bad)}</p>
+      <p>positive {(good/(good+neutral+bad))*100} %</p>
+      
       
     </div>
   )
