1ab30e8d9292ec3681123dc9530255147ec5a1d9
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-05-23 17:56:25 -0400

    Finish 2.5 - move Course component to own file

--- part2/courseinfo2/src/index.js
+++ part2/courseinfo2/src/index.js
@@ -1,50 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce(
-    (accumulator, part) => accumulator + part.exercises, 0
-  )
-  return(
-    <p>Number of exercises {sum}</p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => 
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return(
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>    
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
