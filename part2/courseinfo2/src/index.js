4d2d8e1207a2046abfabd22a089e9ab44b68968f
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-05-23 17:49:31 -0400

    2.2 finished - sum # of exercises using reduce

--- part2/courseinfo2/src/index.js
+++ part2/courseinfo2/src/index.js
@@ -10,7 +10,10 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum = course.parts.reduce(
+    function (accumulator, part) {
+      return accumulator + part.exercises
+    }, 0)
   return(
     <p>Number of exercises {sum}</p>
   ) 
@@ -39,6 +42,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>    
   )
 }
@@ -62,6 +66,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
