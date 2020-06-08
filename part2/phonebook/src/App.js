70362a59fd5603724826ff52fb8fee82e9052856
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-06-07 22:19:56 -0400

    Finish 2.15 - add db.json to gitignore

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -32,8 +32,15 @@ const App = () => {
         name: newName,
         number: newNumber,
       }
-  
-      setPersons(persons.concat(newPerson))
+      
+      axios.post('http://localhost:3001/persons', newPerson)
+           .then(response => {
+            setPersons([...persons, response.data])
+           })
+           .catch(error => {
+             console.log('Error communicating with server.')
+           })
+
       setNewName('')
       setNewNumber('')
       setNameFilter('')
